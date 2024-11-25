22b852406a6aa8773e469da5570e87a993b57c08
ftarhon <florin.titu@gmail.com>
2024-11-25 22:05:39 +0200

    Add translator

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
 from greets import greets
+from translate import Translator
+
+translator = Translator(to_lang='ro')
+
 for g in greets:
-    print(g.title() + "!")
+    print(translator.translate(g).title() + "!")
